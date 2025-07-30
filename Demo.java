e383ff7db744359b9c549f137e8e654d4c4197d4
Adhilc <21bcs612@gecskp.ac.in>
2025-07-30 14:46:39 +0530

    second

--- Demo.java
+++ Demo.java
@@ -1,5 +1,8 @@
 public class Demo
  {
+    public void hello(){
+        System.out.println("say hello")
+    }
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
