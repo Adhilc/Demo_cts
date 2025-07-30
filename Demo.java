0af7a42e07a824f5b4190cb0e8aac059ddcf0f29
Muhnis2002 <muhammedmuhnis99@gmail.com>
2025-07-30 14:48:44 +0530

    Added Hi method

--- Demo.java
+++ Demo.java
@@ -6,4 +6,8 @@ public class Demo
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+    public void hi()
+    {
+        System.out.println("hi");
+    }
 }
